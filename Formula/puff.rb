fcf9d91d897b21a2c6dcc19b1938323ec1b5e4bf
Toru Maesaka <toru@chronohq.com>
2024-08-26 20:44:23 -0700

    ship puff v0.2.0

--- Formula/puff.rb
+++ Formula/puff.rb
@@ -1,9 +1,9 @@
 class Puff < Formula
   desc "CLI tool for generating random values in various formats"
   homepage "https://github.com/chronohq/puff"
-  url "https://github.com/chronohq/puff/releases/download/v0.1.0/puff-0.1.0-darwin-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "37ba983b6407898f55560f34bea2cb03ae833ef2c1e4843f6293857d0c3104c9"
+  url "https://github.com/chronohq/puff/releases/download/v0.2.0/puff-0.2.0-darwin-arm64.tar.gz"
+  version "0.2.0"
+  sha256 "9bb37b2c4a59f9a6e07102b45c1e06919d15087c73f938655ec8fad584bff5bd"
 
   def install
     bin.install "puff"
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Chrono Homebrew Tap
 
-The official [Homebrew](https://brew.sh/) tap for software developed by [Chrono](https://www.chronohq.com/).
+The official [Homebrew](https://brew.sh/) tap for software developed by [Chrono Technologies](https://www.chronohq.com/).
