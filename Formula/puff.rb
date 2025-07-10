dc5a41912eb0fd10a27b364e3265961546b0c97c
Toru Maesaka <toru@chronohq.com>
2025-07-10 14:46:40 -0700

    ship puff v0.6.0

--- Formula/puff.rb
+++ Formula/puff.rb
@@ -1,9 +1,9 @@
 class Puff < Formula
   desc "CLI tool for generating random values in various formats"
   homepage "https://github.com/chronohq/puff"
-  url "https://github.com/chronohq/puff/releases/download/v0.5.1/puff-0.5.1-darwin-arm64.tar.gz"
-  version "0.5.1"
-  sha256 "9e658db22266d91764c851089585b3789544af0ff5872b7f846ff3c8ec7e66d7"
+  url "https://github.com/chronohq/puff/releases/download/v0.6.0/puff-0.6.0-darwin-arm64.tar.gz"
+  version "0.6.0"
+  sha256 "209987a69d58b45238da954034594aff1c9686170429ec00dc6bec960bad57bb"
 
   def install
     bin.install "puff"
