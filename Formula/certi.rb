bcf69f5e862793479b34370b3f104d40b9d7328d
Toru Maesaka <toru@chronohq.com>
2025-09-11 15:31:39 -0700

    ship certi v0.2.0

--- Formula/certi.rb
+++ Formula/certi.rb
@@ -1,9 +1,9 @@
 class Certi < Formula
   desc "TLS certificate chain inspection tool"
   homepage "https://github.com/chronohq/certinspect"
-  url "https://github.com/chronohq/certinspect/releases/download/v0.1.0/certi-0.1.0-darwin-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "6aed937066e65eed438f43ff96b68b1680a935be8b927148e563de7027997488"
+  url "https://github.com/chronohq/certinspect/releases/download/v0.2.0/certi-0.2.0-darwin-arm64.tar.gz"
+  version "0.2.0"
+  sha256 "ffae1a19f72b9785fcd0ecb47912ef6234df8a8a0859def50abca013496e2c11"
 
   def install
     bin.install "certi"
